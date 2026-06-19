9f51db5974d6987ff208d96ad0fd92cd20ac16e0
Antony Melnyk <melnyk.antony@gmail.com>
2026-06-20 00:36:19 +0300

    Add LinkController LinkRequest + LinkResponse LinkEntity Get+Set

--- shortlink/src/main/java/com/petproject/shortlink/entity/LinkEntity.java
+++ shortlink/src/main/java/com/petproject/shortlink/entity/LinkEntity.java
@@ -29,4 +29,44 @@ public class LinkEntity {
 
     @Column(name = "click_count", nullable = false)
     private Long clickCount;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getOriginalUrl() {
+        return originalUrl;
+    }
+
+    public void setOriginalUrl(String originalUrl) {
+        this.originalUrl = originalUrl;
+    }
+
+    public String getShortCode() {
+        return shortCode;
+    }
+
+    public void setShortCode(String shortCode) {
+        this.shortCode = shortCode;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Long getClickCount() {
+        return clickCount;
+    }
+
+    public void setClickCount(Long clickCount) {
+        this.clickCount = clickCount;
+    }
 }
