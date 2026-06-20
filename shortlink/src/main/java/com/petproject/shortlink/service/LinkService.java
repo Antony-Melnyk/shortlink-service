91e106363dce598435ed64f6923cca570e537164
Antony Melnyk <melnyk.antony@gmail.com>
2026-06-20 15:45:25 +0300

    LinkService and LinkServiceTest ShortCodeGenerator - 2

--- shortlink/src/main/java/com/petproject/shortlink/service/LinkService.java
+++ shortlink/src/main/java/com/petproject/shortlink/service/LinkService.java
@@ -1,14 +1,40 @@
 package com.petproject.shortlink.service;
 
+import com.petproject.shortlink.dto.CreateLinkRequest;
+import com.petproject.shortlink.dto.CreateLinkResponse;
+import com.petproject.shortlink.entity.LinkEntity;
 import com.petproject.shortlink.repository.LinkRepository;
+import com.petproject.shortlink.util.ShortCodeGenerator;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
 
 @Service
 public class LinkService {
 
     private final LinkRepository linkRepository;
+    private final ShortCodeGenerator shortCodeGenerator;
 
-    public LinkService(LinkRepository linkRepository) {
+    public LinkService(LinkRepository linkRepository, ShortCodeGenerator shortCodeGenerator) {
         this.linkRepository = linkRepository;
+        this.shortCodeGenerator = shortCodeGenerator;
+    }
+
+    public CreateLinkResponse createLink(CreateLinkRequest request) {
+        String shortCode = shortCodeGenerator.generate();
+
+        LinkEntity link = new LinkEntity();
+        link.setOriginalUrl(request.getUrl());
+        link.setShortCode(shortCode);
+        link.setCreatedAt(LocalDateTime.now());
+        link.setClickCount(0L);
+
+        linkRepository.save(link);
+
+        CreateLinkResponse response = new CreateLinkResponse();
+        response.setShortCode(shortCode);
+        response.setShortUrl("http://localhost:8080/" + shortCode);
+
+        return response;
     }
 }
---
+++ shortlink/src/test/java/com/petproject/shortlink/service/LinkServiceTest.java
@@ -0,0 +1,53 @@
+package com.petproject.shortlink.service;
+
+import com.petproject.shortlink.dto.CreateLinkRequest;
+import com.petproject.shortlink.dto.CreateLinkResponse;
+import com.petproject.shortlink.entity.LinkEntity;
+import com.petproject.shortlink.repository.LinkRepository;
+import com.petproject.shortlink.util.ShortCodeGenerator;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class LinkServiceTest {
+
+    @Mock
+    private LinkRepository linkRepository;
+
+    @Mock
+    private ShortCodeGenerator shortCodeGenerator;
+
+    @InjectMocks
+    private LinkService linkService;
+
+    @Test
+    void shouldCreateShortLink() {
+        CreateLinkRequest request = new CreateLinkRequest();
+        request.setUrl("https://youtube.com");
+
+        when(shortCodeGenerator.generate()).thenReturn("abc123");
+
+        CreateLinkResponse response = linkService.createLink(request);
+
+        assertEquals("abc123", response.getShortCode());
+        assertEquals("http://localhost:8080/abc123", response.getShortUrl());
+
+        ArgumentCaptor<LinkEntity> captor = ArgumentCaptor.forClass(LinkEntity.class);
+
+        verify(linkRepository).save(captor.capture());
+
+        LinkEntity savedLink = captor.getValue();
+
+        assertEquals("https://youtube.com", savedLink.getOriginalUrl());
+        assertEquals("abc123", savedLink.getShortCode());
+        assertEquals(0L, savedLink.getClickCount());
+    }
+}
