6891ae3702b17aa57bb08f37db5676433edc7164
Antony Melnyk <melnyk.antony@gmail.com>
2026-07-03 18:46:45 +0300

    exception flow test via MockMvc. I verify that when LinkService throws LinkNotFoundException, GlobalExceptionHandler returns HTTP 404

---
+++ shortlink/src/test/java/com/petproject/shortlink/controller/RedirectControllerTest.java
@@ -0,0 +1,33 @@
+package com.petproject.shortlink.controller;
+
+import com.petproject.shortlink.exception.LinkNotFoundException;
+import com.petproject.shortlink.service.LinkService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@WebMvcTest(RedirectController.class)
+class RedirectControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockitoBean
+    private LinkService linkService;
+
+    @Test
+    void shouldReturn404WhenShortCodeNotFound() throws Exception {
+        when(linkService.getOriginalUrlAndIncreaseClickCount("abrakadabra"))
+                .thenThrow(new LinkNotFoundException("abrakadabra"));
+
+        mockMvc.perform(get("/abrakadabra"))
+                .andExpect(status().isNotFound())
+                .andExpect(content().string("Link not found with shortCode: abrakadabra"));
+    }
+}
