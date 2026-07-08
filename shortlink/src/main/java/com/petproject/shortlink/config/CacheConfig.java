ff8db36f4b1a3b08a49e98b4bd24fb2802679583
Antony Melnyk <melnyk.antony@gmail.com>
2026-07-09 00:52:16 +0300

    Redis cache

--- shortlink/src/main/java/com/petproject/shortlink/config/CacheConfig.java
+++ shortlink/src/main/java/com/petproject/shortlink/config/CacheConfig.java
@@ -1,15 +1,18 @@
 package com.petproject.shortlink.config;
 
 import org.springframework.cache.CacheManager;
-import org.springframework.cache.concurrent.ConcurrentMapCacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
 
 @Configuration
 public class CacheConfig {
 
     @Bean
-    public CacheManager cacheManager() {
-        return new ConcurrentMapCacheManager("links");
+    public CacheManager cacheManager(RedisConnectionFactory redisConnectionFactory) {
+        return RedisCacheManager
+                .builder(redisConnectionFactory)
+                .build();
     }
 }
